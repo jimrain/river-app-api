f8b50c017dc31e20a1b1efd5faf92bcfb152f998
jimrain <jim@rainville.net>
2026-01-06 14:08:21 -0800

    Adding health check API and turning on pagination

--- app/app/settings.py
+++ app/app/settings.py
@@ -146,4 +146,6 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
