56925accf8f54d87814b1f45ed27dae7899abd1d
jimrain <jim@rainville.net>
2026-01-09 07:17:06 -0800

    Fixing lint errors

--- app/river/views.py
+++ app/river/views.py
@@ -4,7 +4,7 @@ Views for the River APIs.
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from django_filters import rest_framework as filters
+# from django_filters import rest_framework as filters
 from core.models import River
 from core.permissions import IsOwnerOrReadOnly
 from river import serializers
